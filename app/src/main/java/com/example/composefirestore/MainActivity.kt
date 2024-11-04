c4fb48c693f2ece4723c9fa4d487350122418002
hyh123466 <austin061910@gmail.com>
2024-11-04 15:29:23 +0800

    v3_出生體重輸入

--- app/src/main/java/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/com/example/composefirestore/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Button
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.text.input.KeyboardType
 
 
 class MainActivity : ComponentActivity() {
@@ -52,6 +53,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Birth(m:Modifier){
     var userName by remember { mutableStateOf("黃鎰弘")}
+    var userWeight by remember { mutableStateOf(3900)}
 
     Column {
         TextField(
@@ -62,14 +64,28 @@ fun Birth(m:Modifier){
             modifier = m,
             label = { Text("姓名") },
             placeholder = { Text("請輸入您的姓名") }
-
-
         )
 
-        Text("您輸入的姓名是：$userName")
+             TextField(
+                    value = userWeight.toString(),
+            onValueChange = { newText ->
+                if (newText == ""){
+                    userWeight = 0
+                }
+                else{userWeight = newText.toInt()}
+            },
+            label = { Text("出生體重") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+        )
+
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
     }
 
-}
+
+    }
+
+
 
 
 
